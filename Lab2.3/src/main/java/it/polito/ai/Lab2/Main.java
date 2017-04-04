a33a3c941311bdf8e0ab678ced2f9b8e1cc18f32
matti <matti@DESKTOP-UUNQCL5>
2017-04-04 13:08:05 +0200

    added db setup; fixed db connection; added line query; added alpha busline query;

--- Lab2.3/src/main/java/it/polito/ai/Lab2/Main.java
+++ Lab2.3/src/main/java/it/polito/ai/Lab2/Main.java
@@ -9,13 +9,11 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 import java.sql.*;
-import java.util.Properties;
-import org.postgis.*;
-//import org.postgis.DriverWrapper;
 
 public class Main {
 	private static PreparedStatement preparedStatementStop = null;
 	private static PreparedStatement preparedStatementLine = null;
+	private static PreparedStatement preparedStatementStopLine = null;
 	
 	static {
 		try{
@@ -26,11 +24,60 @@ public class Main {
 		}
 	}
 	
+	/*
+	 * https://www.postgresql.org/docs/8.2/static/errcodes-appendix.html
+	 * postgres error code meaning
+	 */
+	public static Connection setupAndConnect() throws SQLException {
+		
+		Connection c = null;
+		try {
+			c = DriverManager.getConnection("jdbc:postgresql://localhost:5432/", "postgres", "ai-user-password");
+		}
+		catch (org.postgresql.util.PSQLException e)
+		{
+			c = DriverManager.getConnection("jdbc:postgresql://192.168.99.100:5432/", "postgres", "ai-user-password");
+		}
+		String create_db = "CREATE DATABASE trasporti;";
+		String create_tables = " create table if not exists BusLine (line varchar(20) not null, description varchar(255), primary key (line));" +
+				"create table if not exists BusStop (id varchar(20) not null, name varchar(255) not null, lat double precision, lng double precision, primary key (id));" + 
+				"create table if not exists BusLineStop (stopId varchar(20) not null, lineId varchar(20) not null, seqenceNumber smallint not null, primary key(stopId, lineId), foreign key (stopId) references BusStop(id), foreign key (lineId) references BusLine(line));";
+		
+		Statement s1 = c.createStatement();
+		
+		try{ 
+			s1.executeUpdate(create_db);
+		}
+		catch (org.postgresql.util.PSQLException e)
+		{
+			if (e.getSQLState().equals("42P04") == false)
+				throw e;
+			System.out.println("Database already exists.");
+			return connect();
+		}
+		finally
+		{
+			c.close();
+		}
+		Connection cdb = connect();
+		Statement s2 = cdb.createStatement();
+		s2.executeUpdate(create_tables);
+		System.out.println("Database created.");
+		return cdb;
+	}
+	
 	public static Connection connect() throws SQLException{
 		String url = "jdbc:postgresql://localhost:5432/trasporti"; // to specify in -p
 		
-		Connection conn = DriverManager.getConnection(url, "postgres", "ai-user-password");
-		
+		Connection conn = null;
+		try {
+			conn = DriverManager.getConnection(url, "postgres", "ai-user-password");
+		}
+		catch (org.postgresql.util.PSQLException e)
+		{
+			// probably mattia's pc? :)
+			conn = DriverManager.getConnection("jdbc:postgresql://192.168.99.100:5432/trasporti", "postgres", "ai-user-password");				
+		}
 		return conn;
 	}
 	
@@ -51,19 +98,60 @@ public class Main {
 		}
 	}
 	
-	public static void executeQueryLine(JSONObject l){
+	public static void executeQueryLine(Object l) throws SQLException{
+		JSONObject jsonLine = (JSONObject) l;
+		preparedStatementLine.setString(1, (String) jsonLine.get("line"));
+		preparedStatementLine.setString(2, (String) jsonLine.get("desc"));
+
+		try {
+			preparedStatementLine.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void executeQueryStopLine(Object l) throws SQLException{
+		JSONObject jsonLine = (JSONObject) l;
+		/*
+		 * 1: bus stop
+		 * 2: line id
+		 * 3: seq(u)ence number
+		 * 
+		 */
+		preparedStatementStopLine.setString(2, (String) jsonLine.get("line"));
+		System.out.println("################# DEBUG: " + jsonLine.get("line").toString());
+		JSONArray stops = (JSONArray) jsonLine.get("stops");
+		short seq = 0;
+		for (Object stop: stops)
+		{
+			seq++;
+			String jsonStop = (String) stop;
+			System.out.println("# stop " + jsonStop + " " + seq);
+			preparedStatementStopLine.setString(1, jsonStop);
+			preparedStatementStopLine.setShort(3, seq);
+			try {
+				preparedStatementStopLine.executeUpdate();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
 		
 	}
 	
 	public static void main(String args[]) throws FileNotFoundException, IOException, ParseException, SQLException{
 		
-		Connection conn = connect();
+		Connection conn = setupAndConnect();
+//		conn.close();
+//		System.exit(0);
 		
 		String queryStop = "insert into BusStop(id, name, lat, lng) values(?, ?, ?, ?)";
 		preparedStatementStop = conn.prepareStatement(queryStop);
 		
 		String queryLine = "insert into BusLine(line, description) values(?, ?)";
 		preparedStatementLine = conn.prepareStatement(queryLine);
+		
+		String queryStopLine = "insert into BusLineStop(stopId, lineId, seqenceNumber) values(?, ?, ?)";
+		preparedStatementStopLine = conn.prepareStatement(queryStopLine);
 		
 		JSONParser parser = new JSONParser();
 		
@@ -73,10 +161,51 @@ public class Main {
 
 		JSONArray lines = (JSONArray) jsonObject.get("lines");
 		JSONArray stops = (JSONArray) jsonObject.get("stops");
-		
-		for(Object stop : stops){
-			executeQueryStop(stop);
+		// insert every stop into the db
+		try {
+			for(Object stop : stops)
+				executeQueryStop(stop);
 		}
+		catch (Exception e)
+		{
+			if (conn != null)
+				conn.close();
+			System.out.println("Abort. " + e);
+			System.exit(2);
+		}
+		// insert every line into the db
+		try {
+			for(Object line : lines)
+				executeQueryLine(line);
+		}
+		catch (Exception e)
+		{
+			if (conn != null)
+				conn.close();
+			System.out.println("Abort. " + e);
+			System.exit(3);
+		}
+		// finally insert the line stop sequence
+		try {
+			for(Object line : lines)
+			{
+				// TODO solve this
+				// executeQueryStopLine(line);
+				System.out.println("Too much.");
+				break;
+			}
+				
+		}
+		catch(Exception e)
+		{
+			System.out.println("Abort. " + e);
+		}
+		finally
+		{
+			conn.close();
+		}
+		System.out.println("End.");
+		
 		/*
 		for (Object line : lines){
 			JSONObject jsonLine = (JSONObject) line;
@@ -88,305 +217,4 @@ public class Main {
 		}
 		*/
 	}
-/*
-    private static final Driver JDBC_DRIVER = new org.hsqldb.jdbcDriver();
-    private static final String DB_URL = "jdbc:hsqldb:mem:sqlproc";
-    private static final String DB_USER = "sa";
-    private static final String DB_PASSWORD = "";
-    private static final SqlFeature DB_TYPE = SqlFeature.HSQLDB;
-    private static final String DB_DDL = "hsqldb.ddl";
-    private static final String[] DB_CLEAR = null;
-
-    private final Logger logger = LoggerFactory.getLogger(getClass());
-
-    private Connection connection;
-    private SqlSessionFactory sessionFactory;
-    private JdbcEngineFactory sqlFactory;
-    private List<String> ddls;
-
-    static {
-        try {
-            DriverManager.registerDriver(JDBC_DRIVER);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public Main() throws SQLException {
-        JdbcEngineFactory factory = new JdbcEngineFactory();
-        factory.setMetaFilesNames("statements.meta");
-        factory.setFilter(DB_TYPE);
-        factory.setValidatorFactory(new SampleValidator.SampleValidatorFactory());
-        this.sqlFactory = factory;
-
-        ddls = DDLLoader.getDDLs(this.getClass(), DB_DDL);
-        connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
-        sessionFactory = new JdbcSessionFactory(connection);
-
-        contactDao = new ContactDao(sqlFactory, sessionFactory);
-        personDao = new PersonDao(sqlFactory, sessionFactory);
-        anHourBeforeDao = new AnHourBeforeDao(sqlFactory, sessionFactory);
-        newPersonDao = new NewPersonDao(sqlFactory, sessionFactory);
-        newPersonRetRsDao = new NewPersonRetRsDao(sqlFactory, sessionFactory);
-    }
-
-    public void setupDb() throws SQLException {
-        SqlSession sqlSession = sessionFactory.getSqlSession();
-        sqlSession.executeBatch((DB_CLEAR != null) ? DB_CLEAR : ddls.toArray(new String[0]));
-    }
-
-    private ContactDao contactDao;
-    private PersonDao personDao;
-    private AnHourBeforeDao anHourBeforeDao;
-    private NewPersonDao newPersonDao;
-    private NewPersonRetRsDao newPersonRetRsDao;
-
-    public Person insertPersonContacts(Person person, Contact... contacts) {
-        Person p = personDao.insert(person, new SqlStandardControl().setProcessingId("insertPerson"));
-        for (Contact contact : contacts) {
-            Contact c = contactDao.insert(contact._setPerson(p),
-                    new SqlStandardControl().setProcessingId("insertContact"));
-            p.getContacts().add(c);
-        }
-        return p;
-    }
-
-    public Person getPerson(Long id, Person.Association... associations) {
-        Person person = new Person()._setId(id)._setInit_(associations);
-        SqlStandardControl ssc = new SqlStandardControl();
-        if (associations != null) {
-            Set<String> set = new TreeSet<String>();
-            for (Person.Association association : associations)
-                set.add(association.name());
-            int hashCode = set.hashCode();
-            if (hashCode < 0)
-                ssc.setProcessingId("getPersonId__" + (-set.hashCode()));
-            else
-                ssc.setProcessingId("getPersonId_" + set.hashCode());
-        }
-        return personDao.get(person, ssc);
-    }
-
-    public void run(boolean dynamic) throws Exception {
-        setupDb();
-
-        Person person, p;
-        Contact contact, c;
-        int count;
-        List<Person> list;
-        List<Contact> listc;
-
-        // insert
-        Person jan = insertPersonContacts(new Person("Jan", "Jansky", PersonGender.MALE),
-                new Contact()._setAddress("Jan address 1")._setPhoneNumber("111-222-3333")._setType(ContactType.HOME));
-        p = getPerson(jan.getId(), Person.Association.contacts);
-        Assert.assertEquals("Jan", p.getFirstName());
-        Assert.assertEquals("Jan address 1", p.getContacts().get(0).getAddress());
-
-        Person janik = insertPersonContacts(new Person("Janik", "Janicek", PersonGender.MALE), new Contact()
-                ._setAddress("Janik address 1")._setType(ContactType.BUSINESS));
-        p = getPerson(janik.getId(), Person.Association.contacts);
-        Assert.assertEquals("Janik", p.getFirstName());
-        Assert.assertEquals("Janik address 1", p.getContacts().get(0).getAddress());
-
-        Person honza = insertPersonContacts(new Person("Honza", "Honzovsky", PersonGender.MALE), new Contact()
-                ._setAddress("Honza address 1")._setType(ContactType.HOME), new Contact()
-                ._setAddress("Honza address 2")._setType(ContactType.BUSINESS));
-        p = getPerson(honza.getId(), Person.Association.contacts);
-        Assert.assertEquals("Honza", p.getFirstName());
-        Assert.assertEquals("Honza address 2", p.getContacts().get(1).getAddress());
-
-        Person honzik = insertPersonContacts(new Person("Honzik", "Honzicek", PersonGender.MALE));
-        p = getPerson(honzik.getId(), Person.Association.contacts);
-        Assert.assertEquals("Honzik", p.getFirstName());
-        Assert.assertEquals(0, p.getContacts().size());
-
-        Person andrej = insertPersonContacts(
-                new Person("Andrej", "Andrejcek", PersonGender.MALE)._setSsn("123456789"),
-                new Contact()._setAddress("Andrej address 1")._setPhoneNumber("444-555-6666")
-                        ._setType(ContactType.BUSINESS));
-        p = getPerson(andrej.getId(), Person.Association.contacts);
-        Assert.assertEquals("Andrej", p.getFirstName());
-        Assert.assertEquals("Andrej address 1", p.getContacts().get(0).getAddress());
-
-        // update
-        person = new Person();
-        person.setId(andrej.getId());
-        person.setFirstName("Andrejik");
-        Date age = getAge(1962, 5, 19);
-        person.setDateOfBirth(age);
-        count = personDao.update(person);
-        Assert.assertEquals(1, count);
-        p = getPerson(andrej.getId());
-        if (dynamic)
-            Assert.assertNull(p.getDateOfBirth());
-        else
-            Assert.assertEquals(age, p.getDateOfBirth());
-
-        // get & update person with null values
-        p = getPerson(andrej.getId());
-        Assert.assertNotNull(p);
-        Assert.assertEquals("Andrejik", p.getFirstName());
-        Assert.assertEquals("Andrejcek", p.getLastName());
-        Assert.assertEquals("123456789", p.getSsn());
-        Assert.assertEquals(PersonGender.MALE, p.getGender());
-        Assert.assertTrue(p.getContacts().size() == 0);
-
-        person = new Person();
-        person.setId(andrej.getId());
-        person.setFirstName("Andriosa");
-        person.setNull_(Person.Attribute.ssn);
-        count = personDao.update(person);
-        Assert.assertEquals(1, count);
-
-        // get person with associations
-        p = getPerson(andrej.getId(), Person.Association.contacts);
-        Assert.assertNotNull(p);
-        Assert.assertEquals("Andriosa", p.getFirstName());
-        Assert.assertEquals("Andrejcek", p.getLastName());
-        Assert.assertNull(p.getSsn());
-        Assert.assertEquals(1, p.getContacts().size());
-        Assert.assertEquals("Andrej address 1", p.getContacts().get(0).getAddress());
-        Assert.assertEquals("444-555-6666", p.getContacts().get(0).getPhoneNumber());
-
-        // list people with associations
-        list = personDao.list(null);
-        Assert.assertEquals(5, list.size());
-        person = new Person();
-        person.setFirstName("XXX");
-        list = personDao.list(person);
-        Assert.assertEquals(0, list.size());
-        person.setFirstName("Jan");
-        person.setInit_(Person.Association.contacts);
-        list = personDao.list(person);
-        person = new Person();
-        person.setInit_(Person.Association.contacts);
-        list = personDao.list(person, new SqlStandardControl().setDescOrder(Person.ORDER_BY_ID));
-        Assert.assertEquals(5, list.size());
-        Assert.assertEquals("Honzicek", list.get(1).getLastName());
-        list = personDao.list(person, new SqlStandardControl().setAscOrder(Person.ORDER_BY_LAST_NAME));
-        Assert.assertEquals(5, list.size());
-        Assert.assertEquals("Honzovsky", list.get(2).getLastName());
-        person = new Person();
-        list = personDao.list(person, new SqlStandardControl().setAscOrder(Person.ORDER_BY_LAST_NAME).setMaxResults(2));
-        Assert.assertEquals(2, list.size());
-
-        // count
-        count = personDao.count(null);
-        Assert.assertEquals(5, count);
-        person = new Person();
-        person.setFirstName("Jan");
-        count = personDao.count(person);
-        Assert.assertEquals(2, count);
-
-        // operators
-        contact = new Contact();
-        contact.setPhoneNumber("444-555-6666");
-        listc = contactDao.list(contact);
-        Assert.assertEquals(1, listc.size());
-        Assert.assertEquals("444-555-6666", listc.get(0).getPhoneNumber());
-        contact.setOp_("<>", Contact.OpAttribute.phoneNumber);
-        listc = contactDao.list(contact);
-        Assert.assertEquals(1, listc.size());
-        Assert.assertEquals("111-222-3333", listc.get(0).getPhoneNumber());
-        contact = new Contact();
-        contact.setNullOp_(Contact.OpAttribute.phoneNumber);
-        count = contactDao.count(contact);
-        Assert.assertEquals(3, count);
-
-        // validation
-        contact = new Contact();
-        contact.setPhoneNumber("444-555-6666");
-        listc = contactDao.list(contact);
-        c = listc.get(0);
-        c.setPhoneNumber("12345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901");
-        try {
-            contactDao.update(c);
-            Assert.fail();
-        } catch (SqlValidationException ex) {
-            logger.warn(ex.getMessage());
-        }
-
-        // delete
-        count = personDao.delete(jan);
-        Assert.assertEquals(1, count);
-
-        // function
-        AnHourBefore anHourBefore = new AnHourBefore();
-        anHourBefore.setT(new java.sql.Timestamp(new Date().getTime()));
-        java.sql.Timestamp result = anHourBeforeDao.anHourBefore(anHourBefore);
-        Assert.assertNotNull(result);
-
-        // procedures
-        NewPerson newPerson = new NewPerson();
-        newPerson.setFirstName("Maruska");
-        newPerson.setLastName("Maruskova");
-        newPerson.setSsn("999888777");
-        newPerson.setDateOfBirth(getAge(1969, 11, 1));
-        newPerson.setGender(PersonGender.FEMALE.getValue());
-        newPersonDao.newPerson(newPerson);
-        Assert.assertNotNull(newPerson.getNewid());
-
-        NewPersonRetRs newPersonRetRs = new NewPersonRetRs();
-        newPersonRetRs.setFirstName("Beruska");
-        newPersonRetRs.setLastName("Beruskova");
-        newPersonRetRs.setSsn("888777666");
-        newPersonRetRs.setDateOfBirth(getAge(1969, 1, 21));
-        newPersonRetRs.setGender(PersonGender.FEMALE.getValue());
-        list = newPersonRetRsDao.newPersonRetRs(newPersonRetRs);
-        Assert.assertNotNull(list);
-        Assert.assertEquals(1, list.size());
-        Assert.assertNotNull(list.get(0).getId());
-    }
-
-    public java.sql.Timestamp getAge(int year, int month, int day) {
-        Calendar birthDay = Calendar.getInstance();
-        birthDay.set(Calendar.YEAR, year);
-        birthDay.set(Calendar.MONTH, month);
-        birthDay.set(Calendar.DAY_OF_MONTH, day);
-        birthDay.set(Calendar.HOUR_OF_DAY, 0);
-        birthDay.set(Calendar.MINUTE, 0);
-        birthDay.set(Calendar.SECOND, 0);
-        birthDay.set(Calendar.MILLISECOND, 0);
-        return new java.sql.Timestamp(birthDay.getTime().getTime());
-    }
-
-    private String SQL_UPDATE_PERSON = "update %%PERSON " + "{= set " + "{ ,%FIRST_NAME = :firstName(call=isDef) } "
-            + "{ ,%LAST_NAME = :lastName(call=isDef) } " + "{ ,%DATE_OF_BIRTH = NULL }"
-            + "{ ,%GENDER = :gender(call=isDef) } " + "{ ,%SSN = :ssn(call=isDef) } " + "} " + "{= where "
-            + "{& %ID = :id(!empty) } " + "}";
-
-    private SqlCrudEngine updatePersonEngine;
-
-    public void modifyPersonUpdate(boolean dynamic) {
-        if (this.updatePersonEngine == null) {
-            this.updatePersonEngine = sqlFactory.getCrudEngine("UPDATE_PERSON");
-            logger.info("DEFAULT SQL ENGINE " + this.updatePersonEngine);
-        }
-        if (dynamic) {
-            SqlCrudEngine updatePersonEngine = sqlFactory.getDynamicCrudEngine("UPDATE_PERSON", SQL_UPDATE_PERSON);
-            logger.info("DYNAMIC SQL ENGINE " + updatePersonEngine);
-            Assert.assertNotSame(this.updatePersonEngine, updatePersonEngine);
-            Assert.assertSame(updatePersonEngine, sqlFactory.getCrudEngine("UPDATE_PERSON"));
-        } else {
-            SqlCrudEngine updatePersonEngine = sqlFactory.getStaticCrudEngine("UPDATE_PERSON");
-            logger.info("STATIC SQL ENGINE " + this.updatePersonEngine);
-            Assert.assertSame(this.updatePersonEngine, updatePersonEngine);
-            Assert.assertSame(updatePersonEngine, sqlFactory.getCrudEngine("UPDATE_PERSON"));
-        }
-    }
-
-    public static void main(String[] args) throws Exception {
-        Main main = new Main();
-        main.modifyPersonUpdate(false);
-        main.run(false);
-        main.modifyPersonUpdate(true);
-        main.run(true);
-        main.sqlFactory.setLazyInit(true);
-        main.modifyPersonUpdate(false);
-        main.run(false);
-        main.modifyPersonUpdate(true);
-        main.run(true);
-    }
-    */
 }

6b18ce93763079b209b0b947f20a3be24e8dce6d
Daniele Magurano <daniele.magurano@tiscali.it>
2017-05-20 19:44:49 +0200

    loadPath test

--- Lab5/app.js
+++ Lab5/app.js
@@ -1,4 +1,4 @@
-var app = angular.module('App', ['ngRoute', 'ngResource'])
+var app = angular.module('App', ['ngRoute', 'ngResource', 'ui-leaflet'])
 
 app.config(function ($routeProvider, $locationProvider) {
     $routeProvider
@@ -9,7 +9,7 @@ app.config(function ($routeProvider, $locationProvider) {
         })
         .when('/map', {
             templateUrl: 'map.html',
-            controller: 'SimpleMapController'
+            controller: 'MapController'
         })
         .otherwise({ redirectTo: "/" });
 
@@ -23,23 +23,46 @@ app.controller('MainCtrl', ['$scope', 'DataProvider',
     }
 ]);
 
-app.controller("SimpleMapController", [ '$scope', function($scope) {
+app.controller("MapController", [ '$scope', 'DataProvider', function($scope, DataProvider) {
     angular.extend($scope, {
-        defaults: {
-            scrollWheelZoom: false
-        }
+        turin: {
+                    lat: 45.07,
+                    lng: 7.69,
+                    zoom: 13
+                },
+        paths: DataProvider.loadPath()        
     });
 }]);
 
-app.factory('DataProvider', ['Linee',
-    function (linee) {
+app.factory('DataProvider', ['Linee', '$filter',
+    function (linee,$filter) {
         return {
             load : function () { return linee.lines; },
             query : function (id) {
-                linee.lines.forEach( function (line) {
+            linee.lines.forEach( function (line) {
                 if (line.line == id)
                     return line;
-            })}
+            })},
+            loadPath : function () { 
+                var paths = {
+                    p1 : {
+                        type : "polyline",
+                        weight: 4,
+                        color: 'red',
+                        message: "<h3>Line: METRO</h3>",
+                        latlngs : []
+                    }
+                };
+                
+                linee.lines[0].stops.forEach( function (stop) {
+                    var point = new Object();
+                    var stop = $filter('filter')(linee.stops, function (s) {return s.id === stop;})[0];
+                    point.lat = stop.latLng[0];
+                    point.lng = stop.latLng[1];
+                    paths.p1.latlngs.push(point)
+                });
+                return paths;
+            }
         };
     }
 ]);
